933cdb0462beac142ac9ba64b4ad9faabec6601b
ElvisDiazLara <46111283+ElvisDiazLara@users.noreply.github.com>
2020-08-01 11:57:46 -0500

    Acciones rapidas

--- app/src/main/java/com/example/notas/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/notas/activities/CreateNoteActivity.java
@@ -128,6 +128,21 @@ public class CreateNoteActivity extends AppCompatActivity {
             }
         });
 
+        if (getIntent().getBooleanExtra("isFromQuickActions", false)){
+            String type = getIntent().getStringExtra("quickActionType");
+            if (type != null){
+                if (type.equals("image")){
+                    selectedImagePath = getIntent().getStringExtra("imagePath");
+                    imageNote.setImageBitmap(BitmapFactory.decodeFile(selectedImagePath));
+                    imageNote.setVisibility(View.VISIBLE);
+                    findViewById(R.id.imageRemoveImage).setVisibility(View.VISIBLE);
+                }else if (type.equals("URL")){
+                    textwebURL.setText(getIntent().getStringExtra("URL"));
+                    layoutWebURL.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+
         initMiscellaneous();
         setSubtitleIndicatorColor();
 
