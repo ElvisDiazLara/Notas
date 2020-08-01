9e660ddfd14f04d4586327e33db3f40eb06d8e61
ElvisDiazLara <46111283+ElvisDiazLara@users.noreply.github.com>
2020-08-01 10:09:37 -0500

    Buscar Nota

--- app/src/main/java/com/example/notas/activities/MainActivity.java
+++ app/src/main/java/com/example/notas/activities/MainActivity.java
@@ -9,8 +9,11 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 
@@ -64,6 +67,26 @@ public class MainActivity extends AppCompatActivity implements NotesListener {
 
 
         getNotes(REQUEST_CODE_SHOW_NOTES, false);
+
+        EditText inputSearch = findViewById(R.id.inputSearch);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                  notesAdapter.cancelTimer();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                   if (noteList.size() != 0){
+                      notesAdapter.searchNotes(editable.toString());
+                   }
+            }
+        });
     }
 
     @Override
